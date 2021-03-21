086925dd8fc554d4bf91dc14f2e1da9d128bc122
sovlookup <gonorth@qq.com>
2021-03-21 19:26:20 +0800

    v3.5.3

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,9 +1,11 @@
+// no dependency error, wrong require in remote module
 export class DependencyError extends Error {
   constructor(msg: string) {
     super(msg);
   }
 }
 
+// fetch jsfile 404
 export class NotFoundError extends Error {
   constructor(msg: string) {
     super(msg);
